c0f0b06928074dc97514deb32ebd456c37d657a6
Aurélien <git@garnier.dev>
2023-12-25 16:09:37 +0000

    Install prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify queue collab',
-  description: 'Collaborate by adding songs to the Spotify queue',
-}
+  title: "Spotify queue collab",
+  description: "Collaborate by adding songs to the Spotify queue",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={openSans.className}>{children}</body>
     </html>
-  )
+  );
 }
